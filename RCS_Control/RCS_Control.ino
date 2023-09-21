fbd5b7282e170d150d78852af50c91e0cafe8742
Niknut <nicolea3504@gmail.com>
2023-09-21 15:09:39 -0500

    Create RCS_Control.ino

---
+++ RCS_Control/RCS_Control.ino
@@ -0,0 +1,15 @@
+#include <math.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  Serial.println("Hello World");
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
