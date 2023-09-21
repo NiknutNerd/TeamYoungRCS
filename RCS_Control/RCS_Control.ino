8dd0d6bd4df54cda7d6cca168c6d2f182844d5dc
Niknut <nicolea3504@gmail.com>
2023-09-21 15:58:34 -0500

    Update RCS_Control.ino

--- RCS_Control/RCS_Control.ino
+++ RCS_Control/RCS_Control.ino
@@ -2,14 +2,36 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BNO055.h>
 
+int brightLEDPin = 9;
+int switchPin = 16;
+
+unsigned long currentTime;
 
 void setup() {
   // put your setup code here, to run once:
-  Serial.begin(9600);
-  Serial.println("Hello World");
+  pinMode(9,OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  currentTime = millis(); 
+  
 }
+
+class Timer{
+  private:
+    unsigned long startTime;
+    unsigned long timeSinceStart;
+  public:
+    Timer(unsigned long initTime){
+      startTime = initTime;
+    }
+    unsigned long getTime(){
+      unsigned long timeSinceStart = currentTime - startTime;
+      return timeSinceStart;
+    }
+    void resetTime(){
+      startTime = currentTime;
+      timeSinceStart = 0;
+    }
+};
